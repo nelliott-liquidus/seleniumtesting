9cc063ec3f3711931e84fa96b526265019f40036
jus10jones <jjones@liquidus.net>
2019-04-26 11:33:45 -0500

    setting up dev tester

---
+++ src/projects/slplatform/devtester/config.js
@@ -0,0 +1,26 @@
+const BASE_URL = 'https://shoplocal-standalonedev.liquidus.net/sliframetester.html'
+
+const suites = {
+
+  homepageTests: [
+
+    'devtester/storeswitch.js'
+
+  ]
+
+}
+
+const aliases = {
+
+  navMenuItems: {
+
+    ads: 'Ads',
+    categories: 'Categories',
+    savedDeals: 'Saved Deals',
+    more: 'More',
+
+  }
+
+}
+
+export { BASE_URL, suites, aliases }
